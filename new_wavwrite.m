9fd4a9c250eeb8342f1124f194597bf71e7692d3
Nils Westhausen <nils.westhausen@student.jade-hs.de>
2015-04-13 14:25:30 +0200

    Intialisation

---
+++ new_wavwrite.m
@@ -0,0 +1,40 @@
+function y = new_wavwrite()
+% function to do something usefull (fill out)
+% Usage y=new_wavwrite()
+% Input Parameter:
+%	 in_param: 		 Explain the parameter, default values, and units
+% Output Parameter:
+%	 out_param: 	 Explain the parameter, default values, and units
+%------------------------------------------------------------------------ 
+% Example: Provide example here if applicable (one or two lines) 
+
+% Author: Nils L. Westhausen (c) TGM @ Jade Hochschule applied licence see EOF 
+% Source: If the function is based on a scientific paper or a web site, 
+%         provide the citation detail here (with equation no. if applicable)  
+% Version History:
+% Ver. 0.01 initial create (empty) 13-Apr-2015  Initials (eg. JB)
+
+%------------Your function implementation here--------------------------- 
+
+
+
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <2015> Nils L. Westhausen
+% Jade University of Applied Sciences 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files 
+% (the "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
