91bc7fe010a029fe8e7471ad2ad740c9295f5f10
Nils Westhausen <nils.westhausen@student.jade-hs.de>
2015-05-07 17:45:32 +0200

    first implementation test setup﻿

--- new_wavreadTest.m
+++ new_wavreadTest.m
@@ -1,18 +1,92 @@
-% Script to test the function new_wavread.m 
+% Script to test and compare the function new_wavread.m with the "old"
 % Author: Nils L. Westhausen (c) TGM @ Jade Hochschule applied licence see EOF 
 % Version History:
-% Ver. 0.01 initial create (empty) 13-Apr-2015 			 Initials (eg. JB)
+% Ver. 0.01 initial create (empty) 13-Apr-2015 			 NW
+% Ver. 0.08 First Implementation   06-May-2015           NW
 
-clear;
-close all;
-clc;
+audiowrite('test_audio.wav', randn(1,96000).*0.1, 48000)
+%-------------------------------------------------------------------------
+%% Testing error returning with four output arguements
+try
+    [y, fs, nbits, opt] = new_wavread('test_audio.wav');
+    
+catch err
+    switch err.identifier
+        case 'myApp:INargChk'
+            
+        otherwise
+            error('not returning error, when called with four output arguements')
+    end
+end
+%% equality of the old and the new; out:[y,fs,nbits] in: filename
+[y_new, fs_new, nbits_new] = new_wavread('test_audio');
+[y_old, fs_old, nbits_old] = wavread('test_audio');
 
-%------------Your script starts here-------- 
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
 
-%Define your parameters and adjust your function call
-y=new_wavread();
+if fs_old ~= fs_new
+    error('the sampling rate is not the same')
+end
 
+if nbits_old ~= nbits_new
+    error('The bits per sample are not equal')
+end
+%% equality of the old and the new; out:[y,fs] in: filename, 'size'
 
+[siz_new, fs_new] = new_wavread('test_audio','size');
+[siz_old, fs_old] = wavread('test_audio','size');
+
+if siz_old ~= siz_new
+    error('the size struct is not equal')
+end
+
+if fs_old ~= fs_new
+    error('the sampling rate is not the same')
+end
+
+%% equality of the old and the new; out:[y] in: filename, sample
+
+[y_new] = new_wavread('test_audio',800);
+[y_old] = wavread('test_audio',800);
+
+if length(y_new) ~= length(y_old)
+    error('the length of the data is not equal')
+end
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+
+%% equality of the old and the new; out:[y] in: filename, [N1 N2]
+[y_new] = new_wavread('test_audio',[800 60000]);
+[y_old] = wavread('test_audio',[800 60000]);
+
+if length(y_new) ~= length(y_old)
+    error('the length of the data is not equal')
+end
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+
+%% equality of the old and the new; out:[y] in: filename, 'native'
+[y_new] = new_wavread('test_audio','native');
+[y_old] = wavread('test_audio','native');
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+%% equality of the old and the new; out:[y] in: filename, 'double'
+[y_new] = new_wavread('test_audio','double');
+[y_old] = wavread('test_audio','double');
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+%%
+delete('test_audio.wav')
 %--------------------Licence ---------------------------------------------
 % Copyright (c) <2015> Nils L. Westhausen
 % Jade University of Applied Sciences 
--- new_wavwriteTest.m
+++ new_wavwriteTest.m
@@ -1,17 +1,118 @@
-% Script to test the function new_wavwrite.m 
+% Script to test and compare the function new_wavwrite.m with the "old"
+% wavwrite
 % Author: Nils L. Westhausen (c) TGM @ Jade Hochschule applied licence see EOF 
 % Version History:
-% Ver. 0.01 initial create (empty) 13-Apr-2015 			 Initials (eg. JB)
+% Ver. 0.01 initial create (empty) 13-Apr-2015 			 NW
+% Ver. 0.08 First Implementation   06-May-2015           NW
+%clear;
+%close all;
+%clc;
+%
+%------------------------------------------------------------------------- 
+%
+%% Testing of Error returning by calling only with one arguement
+data = randn(1,48000).*0.2;
+try
+    new_wavwrite(data);
+    
+catch err
+    switch err.identifier
+        case 'myApp:argChk1'
+            
+        otherwise
+            error('not returning error, when called only with one input argument')
+    end
+end
 
-clear;
-close all;
-clc;
+%% Testing of Error returning by calling with two arguements
+data = randn(1,48000).*0.2;
+filename = 'test.wav';
+try
+    new_wavwrite(data,filename);
+    
+catch err
+    switch err.identifier
+        case 'myApp:argChk2'
+            
+        otherwise
+            error('not returning error, when called with two input arguments')
+    end
+end
 
-%------------Your script starts here-------- 
+%% Testing of writing without .wav extesion
+data = randn(1,48000).*0.2;
+filename = 'test';
+fs =48000;
+try
+    new_wavwrite(data,fs,filename);
+    
+catch err
+    error('writing without .wav extesion not possible')
+    
+end
 
-%Define your parameters and adjust your function call
-y=new_wavwrite();
+delete([filename '.wav'])
+%% Testing of the equality of new and old wavwrite for (data,fs,filename)
+data = randn(1,48000).*0.2;
+fs =48000;
+filename1 = 'test1';
+filename2 = 'test2';
 
+wavwrite(data, fs ,filename1)
+new_wavwrite(data, fs, filename2)
+
+[y_old, fs_old] = audioread([filename1 '.wav']);
+[y_new, fs_new] = audioread([filename2 '.wav']);
+
+nbits_old = audioinfo([filename1 '.wav']);
+nbits_old = nbits_old.BitsPerSample;
+nbits_new = audioinfo([filename2 '.wav']);
+nbits_new = nbits_new.BitsPerSample;
+
+if fs_old ~= fs_new
+    error('the sampling rate is not the same')
+end
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+
+if nbits_old ~= nbits_new
+    error('The bits per sample are not equal')
+end
+
+delete([filename1 '.wav'], [filename2 '.wav'])
+%% Testing of the equality of new and old wavwrite for (data,fs, nbits,filename)
+data = randn(1,48000).*0.2;
+fs =48000;
+nbits = 8;
+filename1 = 'test1';
+filename2 = 'test2';
+
+wavwrite(data, fs , nbits,filename1)
+new_wavwrite(data, fs, nbits, filename2)
+
+[y_old, fs_old] = audioread([filename1 '.wav']);
+[y_new, fs_new] = audioread([filename2 '.wav']);
+
+nbits_old = audioinfo([filename1 '.wav']);
+nbits_old = nbits_old.BitsPerSample;
+nbits_new = audioinfo([filename2 '.wav']);
+nbits_new = nbits_new.BitsPerSample;
+
+if fs_old ~= fs_new
+    error('the sampling rate is not the same')
+end
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+
+if nbits_old ~= nbits_new
+    error('The bits per sample are not equal')
+end
+
+delete([filename1 '.wav'], [filename2 '.wav'])
 
 %--------------------Licence ---------------------------------------------
 % Copyright (c) <2015> Nils L. Westhausen
