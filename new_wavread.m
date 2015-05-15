050893dff0c499591f93a1fdaa1174d734470355
Nils Westhausen <nils.westhausen@student.jade-hs.de>
2015-05-15 17:44:01 +0200

    Added the documentation of the original wavread() and some small formatting﻿

--- new_wavread.m
+++ new_wavread.m
@@ -1,32 +1,77 @@
 function [data, fs, nbits, opt] = new_wavread(filename, additional1, additional2)
-% function to to be used like wavread() with same sytax using audioread and
-% audioinfo instead of the origanal wavread(). Please note the struct opt 
-% which exits as ouptut in the wavread() function is not implemnted.
-
-% Usage [data, fs, nbits] = new_wavread(filename, additional1, additional2)
-% Input Parameter:
-%   filename:       name of the file you want to read (string)
-%   additional1:    the additinal arguments can stand for some things, 
-%   additional2:    please read the wavread() documentation for futher 
-%                   information
-% Output Parameter:
-%   data:           vector of audiodata of the wav-file
-%   fs:             sampling frequencie (double) of the wav-file
-%   nbits:          pits per sample (double) of the wav-file
-%   opt:            returns an error if called
-%------------------------------------------------------------------------ 
-
-% Author: Nils L. Westhausen (c) TGM @ Jade Hochschule applied licence see EOF 
+%new_wavread Read Microsoft WAVE (".wav") sound file.
+%   It is a rewritten wavread() by using audioread.
+%   This is the original documentation of wavread().
+%   Please note the struct opt which exits as ouptut in the wavread() 
+%   function is not implemnted.
+%   
+% 
+%   Y=new_wavread(FILE) reads a WAVE file specified by the string FILE,
+%   returning the sampled data in Y. The ".wav" extension is appended
+%   if no extension is given.
+%
+%   [Y,FS,NBITS]=new_wavread(FILE) returns the sample rate (FS) in Hertz
+%   and the number of bits per sample (NBITS) used to encode the
+%   data in the file.
+%
+%   [...]=new_wavread(FILE,N) returns only the first N samples from each
+%       channel in the file.
+%   [...]=WAVREAD(FILE,[N1 N2]) returns only samples N1 through N2 from
+%       each channel in the file.
+%
+%   [Y,...]=new_wavread(...,FMT) specifies the data type format of Y used
+%       to represent samples read from the file.
+%       If FMT='double', Y contains double-precision normalized samples.
+%       If FMT='native', Y contains samples in the native data type
+%       found in the file.  Interpretation of FMT is case-insensitive,
+%       and partial matching is supported.  If omitted, FMT='double'.
+%
+%   SIZ=new_wavread(FILE,'size') returns the size of the audio data contained
+%       in the file in place of the actual audio data, returning the
+%       2-element vector SIZ=[samples channels].
+%
+%   ([Y,FS,NBITS,OPTS]=new_wavread(...) returns a structure OPTS of additional
+%       information contained in the WAV file.  The content of this
+%       structure differs from file to file.  Typical structure fields
+%       include '.fmt' (audio format information) and '.info' (text
+%       which may describe title, author, etc.)
+%       ) !!NOT IMPLEMENTED!!
+%
+%   Output Scaling
+%   The range of values in Y depends on the data format FMT specified.
+%   Some examples of output scaling based on typical bit-widths found
+%   in a WAV file are given below for both 'double' and 'native' formats.
+%   FMT='native'
+%      #Bits   MATLAB data type          Data range
+%      -----   ------------------------- -------------------
+%        8     uint8  (unsigned integer)      0 <= Y <= 255
+%       16     int16  (signed integer)   -32768 <= Y <= +32767
+%       24     int32  (signed integer)    -2^23 <= Y <= 2^23-1
+%       32     single (floating point)     -1.0 <= Y <= +1.0
+%
+%   FMT='double'
+%      #Bits   MATLAB data type          Data range
+%      -----   ------------------------- -------------------
+%       N<32   double                     -1.0 <= Y <  +1.0
+%       N=32   double                     -1.0 <= Y <= +1.0
+%      Note: Values in y might exceed -1.0 or +1.0 for the case of
+%            N=32 bit data samples stored in the WAV file.
+%
+%   Supports multi-channel data, with up to 32 bits per sample.
+%   Supports Microsoft PCM data format only.
+ 
+% Author: Nils L. Westhausen 
+%        (c) TGM @ Jade Hochschule applied licence see LICENCE file.
 %   
 % Version History:
-% Ver. 0.01 initial create (empty) 13-Apr-2015  NW
-% Ver. 0.80 first implementation   19-Apr-2015  NW
-
+% Ver. 0.0.0 initial create (empty) 13-Apr-2015  NW
+% Ver. 1.0.0 first implementation   19-Apr-2015  NW
+% Ver. 1.1.0 new documentation      15-May-2015  NW
 %--------------------------------------------------------------------------
 
 % return error if output opt exists
-if nargout==4
-    error('myApp:INargChk', 'struct opt is not implemented')
+if nargout == 4
+    error('new_wavread:no_opt', 'struct opt is not implemented')
 end
 
 % sets the not inserted arguments to an empty array
@@ -48,61 +93,33 @@ end
 nbits = audioinfo(filename);
 nbits = nbits.BitsPerSample;
 
-
 if isempty(additional1) && isempty(additional2)
-    [data, fs] = audioread(filename);
-       
+    [data, fs] = audioread(filename);     
 end  
   
 if isempty(additional2) && isa(additional1, 'char')
     
     if strcmp(additional1, 'size')
-       siz = audioinfo(filename); 
-       data = [siz.TotalSamples siz.NumChannels];
-       fs = siz.SampleRate;
-       
+        siz = audioinfo(filename); 
+        data = [siz.TotalSamples siz.NumChannels];
+        fs = siz.SampleRate;
     end
-    
-    
     if strcmp(additional1, 'native') || strcmp(additional1, 'double')
-        [data, fs]=audioread(filename, additional1);
-        
-        
+        [data, fs] = audioread(filename, additional1);
     end
 end
-if  isa(additional1,'double') && (length(additional1)<=2) &&...
-                                                not(isempty(additional1))
+
+if  isa(additional1, 'double') && (length(additional1) <= 2) && ...
+        not(isempty(additional1))
     
-    if length(additional1)==1 
-        additional1=[1 additional1];
+    if length(additional1) == 1 
+        additional1 = [1 additional1];
     end
     if isempty(additional2)
-        [data, fs]=audioread(filename,additional1);
+        [data, fs] = audioread(filename, additional1);
     else
-        [data, fs]=audioread(filename,additional1,additional2);
-    end
-    
-end
+        [data, fs] = audioread(filename, additional1, additional2);
+    end 
 end
 
-
-
-%--------------------Licence ---------------------------------------------
-% Copyright (c) <2015> Nils L. Westhausen
-% Jade University of Applied Sciences 
-% Permission is hereby granted, free of charge, to any person obtaining 
-% a copy of this software and associated documentation files 
-% (the "Software"), to deal in the Software without restriction, including 
-% without limitation the rights to use, copy, modify, merge, publish, 
-% distribute, sublicense, and/or sell copies of the Software, and to
-% permit persons to whom the Software is furnished to do so, subject
-% to the following conditions:
-% The above copyright notice and this permission notice shall be included 
-% in all copies or substantial portions of the Software.
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+end
