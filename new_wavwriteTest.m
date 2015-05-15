77dbdbf8b6099dad71cd1687e22db4607788eb6f
Nils Westhausen <nils.westhausen@student.jade-hs.de>
2015-05-15 17:48:38 +0200

    New test for writing without fs. New test names.﻿

--- new_wavwriteTest.m
+++ new_wavwriteTest.m
@@ -1,13 +1,12 @@
 % Script to test and compare the function new_wavwrite.m with the "old"
 % wavwrite
-% Author: Nils L. Westhausen (c) TGM @ Jade Hochschule applied licence see EOF 
+% Author: Nils L. Westhausen 
+%        (c) TGM @ Jade Hochschule applied licence see LICENCE file.
 % Version History:
-% Ver. 0.01 initial create (empty) 13-Apr-2015 			 NW
-% Ver. 0.08 First Implementation   06-May-2015           NW
-%clear;
-%close all;
-%clc;
-%
+% Ver. 0.0.0 initial create (empty) 13-Apr-2015           NW
+% Ver. 1.0.0 First Implementation   06-May-2015           NW
+% Ver. 1.1.0 new testnames          15-May-2015           NW
+% Ver. 1.2.0 added test             15-May-2015           NW
 %------------------------------------------------------------------------- 
 %
 %% Testing of Error returning by calling only with one arguement
@@ -17,25 +16,10 @@ try
     
 catch err
     switch err.identifier
-        case 'myApp:argChk1'
+        case 'new_wavwrite:notEnoughArgs'
             
         otherwise
             error('not returning error, when called only with one input argument')
-    end
-end
-
-%% Testing of Error returning by calling with two arguements
-data = randn(1,48000).*0.2;
-filename = 'test.wav';
-try
-    new_wavwrite(data,filename);
-    
-catch err
-    switch err.identifier
-        case 'myApp:argChk2'
-            
-        otherwise
-            error('not returning error, when called with two input arguments')
     end
 end
 
@@ -52,7 +36,39 @@ catch err
 end
 
 delete([filename '.wav'])
-%% Testing of the equality of new and old wavwrite for (data,fs,filename)
+%% Output of wavwrite(data, filename) should be the same as new_wavwrite(data, filename)
+
+data = randn(1,48000).*0.2;
+
+filename1 = 'test1';
+filename2 = 'test2';
+
+wavwrite(data ,filename1)
+new_wavwrite(data, filename2)
+
+[y_old, fs_old] = audioread([filename1 '.wav']);
+[y_new, fs_new] = audioread([filename2 '.wav']);
+
+nbits_old = audioinfo([filename1 '.wav']);
+nbits_old = nbits_old.BitsPerSample;
+nbits_new = audioinfo([filename2 '.wav']);
+nbits_new = nbits_new.BitsPerSample;
+
+if fs_old ~= fs_new
+    error('the sampling rate is not the same')
+end
+
+if any(find(abs(y_old - y_new)> 0.01))
+    error('the data is not equal')
+end
+
+if nbits_old ~= nbits_new
+    error('The bits per sample are not equal')
+end
+
+delete([filename1 '.wav'], [filename2 '.wav'])
+%% Output of wavwrite(data, fs, filename) should be the same as new_wavwrite(data, fs, filename)
+
 data = randn(1,48000).*0.2;
 fs =48000;
 filename1 = 'test1';
@@ -82,7 +98,8 @@ if nbits_old ~= nbits_new
 end
 
 delete([filename1 '.wav'], [filename2 '.wav'])
-%% Testing of the equality of new and old wavwrite for (data,fs, nbits,filename)
+%% Output of wavwrite(data, fs, nbits, filename) should be the same as new_wavwrite(data, fs, nbits, filename
+
 data = randn(1,48000).*0.2;
 fs =48000;
 nbits = 8;
@@ -114,22 +131,3 @@ end
 
 delete([filename1 '.wav'], [filename2 '.wav'])
 
-%--------------------Licence ---------------------------------------------
-% Copyright (c) <2015> Nils L. Westhausen
-% Jade University of Applied Sciences 
-% Permission is hereby granted, free of charge, to any person obtaining 
-% a copy of this software and associated documentation files 
-% (the "Software"), to deal in the Software without restriction, including 
-% without limitation the rights to use, copy, modify, merge, publish, 
-% distribute, sublicense, and/or sell copies of the Software, and to
-% permit persons to whom the Software is furnished to do so, subject
-% to the following conditions:
-% The above copyright notice and this permission notice shall be included 
-% in all copies or substantial portions of the Software.
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
